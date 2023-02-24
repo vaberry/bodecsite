98f605134e65f037a23360665c106f4881a7beba
vaberry <vincent.berry11@gmail.com>
2023-02-24 12:46:04 -0500

    changed db config

--- bodecsite/settings.py
+++ bodecsite/settings.py
@@ -2,13 +2,13 @@ from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
 import sys
-
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
-# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+# ALLOWED_HOSTS = ['*']
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 INSTALLED_APPS = [
