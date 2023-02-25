8ebe6a62a1d23e10781388d6ead06af8f5b6456d
vaberry <vincent.berry11@gmail.com>
2023-02-25 10:24:10 -0500

    static change

--- bodecsite/settings.py
+++ bodecsite/settings.py
@@ -122,9 +122,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
